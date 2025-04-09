3ef52a30031e665f45709745ddde546220115194
SharmisthaSahoo47 <sharmistha500@gmail.com>
2025-04-09 09:08:21 +0530

    new git.py file has been created

---
+++ git.py
@@ -0,0 +1,15 @@
+# This is day 2 practice of git.
+
+# We are learning to merge two different commits at the RR.
+
+# git status - check the status of your file
+
+# I am adding git.py to staging area then lr then push to RR.
+
+# Git add - add the file to staging
+
+# git log - see the commit updates 
+
+# git push - push to remote repository
+
+
