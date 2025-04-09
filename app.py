8e3f6007b588f92770c394f0560da66c40031c42
SharmisthaSahoo47 <sharmistha500@gmail.com>
2025-04-09 16:18:10 +0530

    updated app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+mport json
+import boto3
+
+def lambda_handler(event, context):
+        client = boto3.client('ec2')
+            response = client.run_instances(
+                            ImageId='ami-0614680123427b75e',
+                                    InstanceType='t2.micro',
+                                            KeyName='aws',
+                                                    MaxCount=1,
+                                                            MinCount=1
+                                                                )
+                return {
+                                'statusCode': 200,
+                                        'body': json.dumps(f"Launched instance ID: {response['Instances'][0]['InstanceId']}")
+                                            }
+
